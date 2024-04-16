cb483fab41f17946bc2e3e920f0c5ab68a806f7b
grishasolakhyan <borsch.tv@mail.ru>
2024-04-16 17:14:10 +1000

    PRNG update

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def middle_square_method():
+    return 0
+
+def middle_multiplication_method():
+    return 0
+
+def linear_congruential_method():
+    return 0
+
+def mixing_method():
+    return 0
