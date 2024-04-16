5165ee51cd48b28cc23d78870f656b1f417a3958
grishasolakhyan <borsch.tv@mail.ru>
2024-04-16 17:46:11 +1000

    PRNG update

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import numpy as np
 
-def middle_square_method():
-    return 0
+class PRNG_methods:
+    def __init__(self):
+        pass
 
-def middle_multiplication_method():
-    return 0
+    def middle_square_method(self, x):
+        x2 = x**2
+        return x2
 
-def linear_congruential_method():
-    return 0
+    def middle_multiplication_method(self):
+        return 0
 
-def mixing_method():
-    return 0
+    def linear_congruential_method(self):
+        return 0
+
+    def mixing_method(self):
+        return 0
+
+method = PRNG_methods()
+num = int(input('Enter the number: '))
+res = method.middle_square_method(num)
+print(res)
