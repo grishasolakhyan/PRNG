2107d3e7dfb4968984312abe296e24b872a64b22
grishasolakhyan <borsch.tv@mail.ru>
2024-04-20 17:55:46 +1000

    PRNG update

--- main.py
+++ main.py
@@ -54,15 +54,12 @@ class PRNG_methods:
 
             if len(str_a2) < 8:
                 str_a2 = (8 - len(str_a2)) * '0' + str_a2
-            print(str_a2)
             str_a2 = str_a2[2:6]
 
             str_a_0 = self.fix_zero(str_a2)
             a2 = int(str_a_0)
 
             a2 = self.fix_loop(main_list, a2)
-            print(f'{str_a2} -> {str_a_0}')
-
             main_list.append(a2 / 10000)
             a1 = a2
 
@@ -74,7 +71,7 @@ class PRNG_methods:
             X_list.append(main_list[i])
             Y_list.append(main_list[N - i - 1])
 
-        return X_list, Y_list
+        return main_list, X_list, Y_list
 
     def middle_multiplication_method(self, iters):
         main_list = []
@@ -104,10 +101,6 @@ class PRNG_methods:
             c2 = int(str_c_0)
 
             c2 = self.fix_loop(main_list, c2)
-            print(f'{str_c2} -> {str_c_0}')
-
-            print(f'{a} x {b} = {c1}')
-
             main_list.append(c2 / 10000)
 
             a = b
@@ -121,7 +114,7 @@ class PRNG_methods:
             X_list.append(main_list[i])
             Y_list.append(main_list[N - i - 1])
 
-        return X_list, Y_list
+        return main_list, X_list, Y_list
 
     def mixing_method(self, iters):
         main_list = []
@@ -167,7 +160,7 @@ class PRNG_methods:
             X_list.append(main_list[i])
             Y_list.append(main_list[N - i - 1])
 
-        return X_list, Y_list
+        return main_list, X_list, Y_list
 
     def linear_congruential_method(self, iters):
         main_list = []
@@ -188,15 +181,85 @@ class PRNG_methods:
             X_list.append(main_list[i])
             Y_list.append(main_list[N - i - 1])
 
-        return X_list, Y_list
+        return main_list, X_list, Y_list
+
+def frequency_analysis_method(arr, n):
+    steps = int(input(f'Enter the number of steps: '))
+
+    if steps == 0:
+        raise Zero()
+    elif steps < 0:
+        raise NegativeNumber()
+
+    st = 1 / steps
+    s_t0 = -st
+    s_t1 = 0
+
+    freq_arr = []
+    ind = []
+
+    for i in range(steps):
+        s_t0 = s_t0 + st
+        s_t1 = s_t1 + st
+        k = 0
+        for j in range(len(arr)):
+            if (s_t0 < arr[j] <= s_t1):
+                k = k + 1
+        freq_arr.append(k)
+
+    for i in range(len(freq_arr)):
+        freq_arr[i] = (freq_arr[i] / n) * 100
+        ind.append(i + 1)
+
+    return freq_arr, ind
+
+def correlation(main_list):
+    ind2=[]
+    R=[]
+    for i in range(1,n):
+        ter=[]
+        for j in range(i):
+            ter.append(main_list[j]*main_list[j+1])
+        R.append((sum(ter)/i)*12-3)
+        ind2.append(i)
+    return R, ind2
+
+
+def expected_value_and_variance(final_array):
+    sum_m = 0
+    M = sum(final_array) / N  # expected value
+
+    mid_final_array = np.mean(final_array)
+    for i in range(len(final_array)):
+        sum_m = sum_m + (mid_final_array - final_array[i]) ** 2
+    D = sum_m / N  # variance
+    return M, D
 
 class Graph:
     def __init__(self):
         pass
 
-    def scatter(self, x, y):
+    def scatter(self, title, x, y, rand1, rand2, freq_arr, ind, corr_arr, ind2):
+        str_N = str(N)
+        figure, axis = plt.subplots(2, 2)
 
-        plt.scatter(x, y, s=8, c='#007dff')
+        axis[0, 0].scatter(x, y, s=8, c='#007dff')
+        axis[0, 0].set_title(title + ' at N=' + str_N)
+        axis[0, 0].set_xlabel('r')
+        axis[0, 0].set_ylabel('r=f(y)')
+
+        axis[0, 1].scatter(rand1, rand2, s=8, c='red')
+        axis[0, 1].set_title('rand()' + ' at N=' + str_N)
+        axis[0, 1].set_xlabel('r')
+        axis[0, 1].set_ylabel('r=f(y)')
+
+        axis[1, 0].plot(ind, freq_arr, c='blue')
+        axis[1, 0].set_title('Frequency analysis' + ' at N=' + str_N)
+        axis[1, 0].set_ylabel('fi, %')
+
+        axis[1, 1].plot(ind2, corr_arr, c='orange')
+        axis[1, 1].set_title("Correlation" + ' at N=' + str_N)
+
         plt.show()
 
         return 0
@@ -209,8 +272,6 @@ def randomize(iters):
     for i in range(iters):
         rand.append(random.randint(0, 9999))
         rand[i] = rand[i] / 10000
-    print(rand)
-    print('\n')
     for i in range(n):
         rand1.append(rand[i])
         rand2.append(rand[iters - i - 1])
@@ -230,15 +291,16 @@ while(number_operation != '0'):
     if(number_operation == '1'):
         method = PRNG_methods()
         try:
-            X_list, Y_list = method.middle_square_method(N)
+            main_list, X_list, Y_list = method.middle_square_method(N)
             rand1, rand2 = randomize(N)
-
-            print(f'{X_list}')
-            print(f'{Y_list}\n')
+            frequency_array, ind = frequency_analysis_method(X_list, n)
+            correlation_array, ind2 = correlation(main_list)
+            M, D = expected_value_and_variance(main_list)
+            title = 'Middle square method'
 
             plot_graph = Graph()
-            graph1 = plot_graph.scatter(X_list, Y_list)
-
+            plot_graph.scatter(title, X_list, Y_list, rand1, rand2, frequency_array, ind, correlation_array, ind2)
+            print(f'{M} and {D}')
 
         except Zero:
             print(f'Wrong number: Zero!\n')
@@ -253,13 +315,14 @@ while(number_operation != '0'):
         Y_list = []
 
         try:
-            X_list, Y_list = method.middle_multiplication_method(N)
-
-            print(f'{X_list}')
-            print(f'{Y_list}\n')
+            main_list, X_list, Y_list = method.middle_multiplication_method(N)
+            rand1, rand2 = randomize(N)
+            frequency_array, ind = frequency_analysis_method(X_list, n)
+            correlation_array, ind2 = correlation(main_list)
+            title = 'Middle multiplication method'
 
             plot_graph = Graph()
-            plot_graph.scatter(X_list, Y_list)
+            plot_graph.scatter(title, X_list, Y_list, rand1, rand2, frequency_array, ind, correlation_array, ind2)
 
         except Zero:
             print(f'Wrong number: Zero!\n')
@@ -273,15 +336,14 @@ while(number_operation != '0'):
         X_list = []
         Y_list = []
         try:
-            X_list, Y_list = method.mixing_method(N)
+            main_list, X_list, Y_list = method.mixing_method(N)
             rand1, rand2 = randomize(N)
-
-            print(f'{X_list}')
-            print(f'{Y_list}\n')
+            frequency_array, ind = frequency_analysis_method(X_list, n)
+            correlation_array, ind2 = correlation(main_list)
+            title = 'Mixing method'
 
             plot_graph = Graph()
-            plot_graph.scatter(X_list, Y_list)
-            plot_graph.scatter(rand1, rand2)
+            plot_graph.scatter(title, X_list, Y_list, rand1, rand2, frequency_array, ind, correlation_array, ind2)
 
         except Zero:
             print(f'Wrong number: Zero!\n')
@@ -292,16 +354,15 @@ while(number_operation != '0'):
 
     elif(number_operation == '4'):
         method = PRNG_methods()
-        X_list = []
-        Y_list = []
 
-        X_list, Y_list = method.linear_congruential_method(N)
-
-        print(f'{X_list}')
-        print(f'{Y_list}\n')
+        main_list, X_list, Y_list = method.linear_congruential_method(N)
+        rand1, rand2 = randomize(N)
+        frequency_array, ind = frequency_analysis_method(X_list, n)
+        correlation_array, ind2 = correlation(main_list)
+        title = 'Linear congruential method'
 
         plot_graph = Graph()
-        plot_graph.scatter(X_list, Y_list)
+        plot_graph.scatter(title, X_list, Y_list, rand1, rand2, frequency_array, ind, correlation_array, ind2)
 
     elif(number_operation == '0'):
         quit()
